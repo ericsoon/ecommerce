423d4974dc2282f52f90d9d33ccaa88716b22ffe
ericsoon <eric.soonsmk@gmail.com>
2022-06-02 19:25:01 -0700

    add layout and navigation bar on top

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import Head from 'next/head'
+import Navbar from './Navbar'
+import Footer from './Footer'
 
-export default function Layout() {
+const Layout = ({ children }) => {
   return (
-    <div>Layout</div>
+    <div className='layout'>
+      <Head>
+        <title>Soon Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-container'>
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
+
+export default Layout
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import Link from "next/link"
+import {AiOutlineShopping} from 'react-icons/ai'
 
-export default function Navbar() {
+const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href = "/">Soon Headphones</Link>
+      </p>
+
+      <button type='button' className='cart-icon' onClick=''>
+        <AiOutlineShopping />
+        <span className='cart-item-qty'>1</span>
+      </button>
+    </div>
   )
 }
+
+export default Navbar
